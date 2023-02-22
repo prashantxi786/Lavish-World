fea7e84107b1d9a8a12b1ecb5eca7b68c74ad332
Prashant Singh <prashantxi786@gmail.com>
2023-02-22 23:08:34 +0530

    added backend boilerplate II

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 require('dotenv').config()
 const {connection}=require('./configs/db')
+const { itemRouter } = require('./routes/item.routes')
 const app = express()
-app.get('/',(req, res) =>{
-    res.send("Hello")
-})
+app.use("/",itemRouter)
 app.listen(process.env.port,async()=>{
     try {
         await connection
