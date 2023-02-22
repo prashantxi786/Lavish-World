f2d18857fa7d9073b6eedcfada991feaca4de436
shahabhi095 <shahabhi095@gmail.com>
2023-02-23 00:01:27 +0530

    Admin side first day layout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import {BrowserRouter} from "react-router-dom"
+import { ChakraProvider } from "@chakra-ui/react";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChakraProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ChakraProvider>
+);
