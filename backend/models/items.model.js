fdc33d15b67f6f32ffa21d513d3c41fadf7abf71
Prashant Singh <prashantxi786@gmail.com>
2023-02-24 00:08:10 +0530

    second commit

--- backend/models/items.model.js
+++ backend/models/items.model.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose')
+const itemSchema=mongoose.Schema({
+    "product-title":String,
+    "product-titles href" :String,
+    "price" :String,
+    "compare-at-price" :String,
+    "w-full" :String
+
+})
+const ItemModel = mongoose.model("item",itemSchema)
+module.exports={ItemModel}
