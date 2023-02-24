644ebcae8f685bf066cd44448f46ec2f01445f00
shahabhi095 <shahabhi095@gmail.com>
2023-02-24 15:29:06 +0530

    Dashboard,Addproduct page updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 
 import Signin from './Components/Auth Components/Signin'
+import Dahsboard from "./Pages/admin/Dahsboard";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
 
- <Allroutes/>
+ <Allroutes/> 
 
     </div>
   );
