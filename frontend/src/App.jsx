43bc1b27e745077e288ed5e0cb47907f982a6270
shahabhi095 <shahabhi095@gmail.com>
2023-02-26 17:43:56 +0530

    redux function written

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 
-// import Navbar from "./Components/HomepageComponents/Navbar"
-// import Navbar from "./Components/Navbar/Navbar";
-// import SmallNavbar from "./Components/Navbar/SmallNavbar"
-// import Footer from "./Components/Footer/Footer"
+
+ import SmallNavbar from "./Components/Navbar/SmallNavbar"
+ import Footer from "./Components/Footer/Footer"
 import "./App.css";
 import Allroutes from "./Routes/Allroutes";
 import { useState } from 'react'
@@ -11,7 +10,7 @@ import Signin from './Components/Auth Components/Signin'
 import ProductsRoutes from "./Routes/ProductsRoutes";
 import New  from "./Pages/ProductPages/New"
 import Dahsboard from "./Pages/admin/Dahsboard";
-
+import Navbar from "./Components/Navbar/Navbar"
 
 
 
@@ -23,12 +22,12 @@ function App() {
     <div className="App">
 
       
-      {/* <div className="big">
+      <div className="big">
         <Navbar />
-      </div>
+      </div> 
       <div className="small">
         <SmallNavbar />
-      </div> */}
+      </div> 
 
 
       <Allroutes/>
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,203 +1,203 @@
-import { AiOutlineShoppingCart, AiOutlineSearch } from "react-icons/ai";
-import { BsPerson } from "react-icons/bs";
-import { FiHeart } from "react-icons/fi"
-import Logo from "../assets/lavish_world_logo.png";
+// import { AiOutlineShoppingCart, AiOutlineSearch } from "react-icons/ai";
+// import { BsPerson } from "react-icons/bs";
+// import { FiHeart } from "react-icons/fi"
+// import Logo from "../assets/lavish_world_logo.png";
 
-import {
-  // Dropdown,
-  Dropdown1,
-  Dropdown2,
-  Dropdown4,
-  Dropdown5,
-  Dropdown6,
-  Dropdown7,
-  Dropdown8,
-  Dropdown3,
-  Dropdown9,
-  Dropdown10,
-  Dropdown11,
-  Dropdown12
-} from "./Dropdown";
-import {
-  Flex,
-  Spacer,
-  Box,
-  Image,
-  Center,
-  ButtonGroup,
-  Button,
-  InputLeftElement,
-  InputGroup,
-  Input,
-  Text,
-} from "@chakra-ui/react";
-import styles from "./Navbar.module.css";
-import { useState } from "react";
+// import {
+//   // Dropdown,
+//   Dropdown1,
+//   Dropdown2,
+//   Dropdown4,
+//   Dropdown5,
+//   Dropdown6,
+//   Dropdown7,
+//   Dropdown8,
+//   Dropdown3,
+//   Dropdown9,
+//   Dropdown10,
+//   Dropdown11,
+//   Dropdown12
+// } from "./Dropdown";
+// import {
+//   Flex,
+//   Spacer,
+//   Box,
+//   Image,
+//   Center,
+//   ButtonGroup,
+//   Button,
+//   InputLeftElement,
+//   InputGroup,
+//   Input,
+//   Text,
+// } from "@chakra-ui/react";
+// import styles from "./Navbar.module.css";
+// import { useState } from "react";
 
 
-const Navbar = ({ display = "flex" }) => {
-  // const [dropdown, setdropdown] = useState(false);
-  const [dropdown1, setdropdown1] = useState(false);
-  const [dropdown2, setdropdown2] = useState(false);
-  const [dropdown3, setdropdown3] = useState(false);
-  const [dropdown4, setdropdown4] = useState(false);
-  const [dropdown5, setdropdown5] = useState(false);
-  const [dropdown6, setdropdown6] = useState(false);
-  const [dropdown7, setdropdown7] = useState(false);
-  const [dropdown8, setdropdown8] = useState(false);
-  const [dropdown9, setdropdown9] = useState(false);
-  const [dropdown10, setdropdown10] = useState(false);
-  const [dropdown11, setdropdown11] = useState(false);
-  const [dropdown12, setdropdown12] = useState(false);
+// const Navbar = ({ display = "flex" }) => {
+//   // const [dropdown, setdropdown] = useState(false);
+//   const [dropdown1, setdropdown1] = useState(false);
+//   const [dropdown2, setdropdown2] = useState(false);
+//   const [dropdown3, setdropdown3] = useState(false);
+//   const [dropdown4, setdropdown4] = useState(false);
+//   const [dropdown5, setdropdown5] = useState(false);
+//   const [dropdown6, setdropdown6] = useState(false);
+//   const [dropdown7, setdropdown7] = useState(false);
+//   const [dropdown8, setdropdown8] = useState(false);
+//   const [dropdown9, setdropdown9] = useState(false);
+//   const [dropdown10, setdropdown10] = useState(false);
+//   const [dropdown11, setdropdown11] = useState(false);
+//   const [dropdown12, setdropdown12] = useState(false);
 
-  return (
-    <>
-      <nav className={styles.nav_1}>
-        <div className={styles.innernav}>
-          <Flex alignItems="center" gap="2" p={1}>
-            <Image width="170px" height="75px" src={Logo} marginLeft="20px" />
-            <Box p="2" display={"flex"}>
-              <InputGroup marginLeft={"100px"}>
-                <InputLeftElement
-                  cursor="pointer"
-                  children=
-                  {<AiOutlineSearch style={{ position: "absolute", left: "560px", fontSize: "22px" }} color="gray.300" />}
-                />
-                <Input
-                  width={"600px"}
-                  type="text"
-                  placeholder="Search"
-                  border={"3px solid #9e9e9e"}
-                />
-              </InputGroup>
-            </Box>
+//   return (
+//     <>
+//       <nav className={styles.nav_1}>
+//         <div className={styles.innernav}>
+//           <Flex alignItems="center" gap="2" p={1}>
+//             <Image width="170px" height="75px" src={Logo} marginLeft="20px" />
+//             <Box p="2" display={"flex"}>
+//               <InputGroup marginLeft={"100px"}>
+//                 <InputLeftElement
+//                   cursor="pointer"
+//                   children=
+//                   {<AiOutlineSearch style={{ position: "absolute", left: "560px", fontSize: "22px" }} color="gray.300" />}
+//                 />
+//                 <Input
+//                   width={"600px"}
+//                   type="text"
+//                   placeholder="Search"
+//                   border={"3px solid #9e9e9e"}
+//                 />
+//               </InputGroup>
+//             </Box>
 
-            <Spacer />
+//             <Spacer />
 
-            <ButtonGroup gap="2" className={styles.rightside}>
-              <Flex>
-                <Center>
-                  <BsPerson className={styles.carticons} />
-                </Center>
-              </Flex>
+//             <ButtonGroup gap="2" className={styles.rightside}>
+//               <Flex>
+//                 <Center>
+//                   <BsPerson className={styles.carticons} />
+//                 </Center>
+//               </Flex>
 
-              <Flex>
-                <Center>
-                  <FiHeart className={styles.carticons} />
-                </Center>
-              </Flex>
+//               <Flex>
+//                 <Center>
+//                   <FiHeart className={styles.carticons} />
+//                 </Center>
+//               </Flex>
 
-              <button className={styles.cartbtn} >
-                <Flex align="center">
-                  <AiOutlineShoppingCart className={styles.carticons1} />
-                </Flex>
-              </button>
-            </ButtonGroup>
-          </Flex>
-        </div>
-      </nav>
-      <nav className={styles.nav_2}>
-        <Flex
-          maxWidth="100%"
-          alignItems="center"
-          gap="45px"
-          justify={"center"}
-          display={display}
-        >
-          <Text>New</Text>
-          <Text
-            onMouseEnter={() => setdropdown1(true)}
-            onMouseLeave={() => setdropdown1(false)}
-          >
-            Massage
-          </Text>
-          <Text
-            onMouseEnter={() => setdropdown2(true)}
-            onMouseLeave={() => setdropdown2(false)}
-          >
-            Wellness
-          </Text>
-          <Text
-            onMouseEnter={() => setdropdown3(true)}
-            onMouseLeave={() => setdropdown3(false)}
-          >
-            Sleep
-          </Text>
-          <Text
-            onMouseEnter={() => setdropdown4(true)}
-            onMouseLeave={() => setdropdown4(false)}
-          >
-            Home
-          </Text>
-          <Text
-            onMouseEnter={() => setdropdown5(true)}
-            onMouseLeave={() => setdropdown5(false)}
-          >
-            Wine & Bar
-          </Text>
-          <Text
-            onMouseEnter={() => setdropdown6(true)}
-            onMouseLeave={() => setdropdown6(false)}
-          >
+//               <button className={styles.cartbtn} >
+//                 <Flex align="center">
+//                   <AiOutlineShoppingCart className={styles.carticons1} />
+//                 </Flex>
+//               </button>
+//             </ButtonGroup>
+//           </Flex>
+//         </div>
+//       </nav>
+//       <nav className={styles.nav_2}>
+//         <Flex
+//           maxWidth="100%"
+//           alignItems="center"
+//           gap="45px"
+//           justify={"center"}
+//           display={display}
+//         >
+//           <Text>New</Text>
+//           <Text
+//             onMouseEnter={() => setdropdown1(true)}
+//             onMouseLeave={() => setdropdown1(false)}
+//           >
+//             Massage
+//           </Text>
+//           <Text
+//             onMouseEnter={() => setdropdown2(true)}
+//             onMouseLeave={() => setdropdown2(false)}
+//           >
+//             Wellness
+//           </Text>
+//           <Text
+//             onMouseEnter={() => setdropdown3(true)}
+//             onMouseLeave={() => setdropdown3(false)}
+//           >
+//             Sleep
+//           </Text>
+//           <Text
+//             onMouseEnter={() => setdropdown4(true)}
+//             onMouseLeave={() => setdropdown4(false)}
+//           >
+//             Home
+//           </Text>
+//           <Text
+//             onMouseEnter={() => setdropdown5(true)}
+//             onMouseLeave={() => setdropdown5(false)}
+//           >
+//             Wine & Bar
+//           </Text>
+//           <Text
+//             onMouseEnter={() => setdropdown6(true)}
+//             onMouseLeave={() => setdropdown6(false)}
+//           >
 
-            Fitness
-          </Text>
-          <Text
-            onMouseEnter={() => setdropdown7(true)}
-            onMouseLeave={() => setdropdown7(false)}
-          >
-            Outdoor
-          </Text>
+//             Fitness
+//           </Text>
+//           <Text
+//             onMouseEnter={() => setdropdown7(true)}
+//             onMouseLeave={() => setdropdown7(false)}
+//           >
+//             Outdoor
+//           </Text>
 
-          <Text
-            onMouseEnter={() => setdropdown8(true)}
-            onMouseLeave={() => setdropdown8(false)}
-          >
-            Travel
-          </Text>
+//           <Text
+//             onMouseEnter={() => setdropdown8(true)}
+//             onMouseLeave={() => setdropdown8(false)}
+//           >
+//             Travel
+//           </Text>
 
-          <Text
-            onMouseEnter={() => setdropdown9(true)}
-            onMouseLeave={() => setdropdown9(false)}
-          >
-            Accessories
-          </Text>
-          <Text
-            onMouseEnter={() => setdropdown10(true)}
-            onMouseLeave={() => setdropdown10(false)}
-          >
-            Entertainment
-          </Text>
-          <Text
-            onMouseEnter={() => setdropdown11(true)}
-            onMouseLeave={() => setdropdown11(false)}
-          >
-            Gifts
-          </Text>
+//           <Text
+//             onMouseEnter={() => setdropdown9(true)}
+//             onMouseLeave={() => setdropdown9(false)}
+//           >
+//             Accessories
+//           </Text>
+//           <Text
+//             onMouseEnter={() => setdropdown10(true)}
+//             onMouseLeave={() => setdropdown10(false)}
+//           >
+//             Entertainment
+//           </Text>
+//           <Text
+//             onMouseEnter={() => setdropdown11(true)}
+//             onMouseLeave={() => setdropdown11(false)}
+//           >
+//             Gifts
+//           </Text>
 
-          <Text
-            onMouseEnter={() => setdropdown12(true)}
-            onMouseLeave={() => setdropdown12(false)}
-          >
-            Sale
-          </Text>
-        </Flex>
-      </nav>
-      {/* {dropdown ? <Dropdown setdropdown={setdropdown} /> : null} */}
-      {dropdown1 ? <Dropdown1 setdropdown1={setdropdown1} /> : null}
-      {dropdown2 ? <Dropdown2 setdropdown2={setdropdown2} /> : null}
-      {dropdown3 ? <Dropdown3 setdropdown3={setdropdown3} /> : null}
-      {dropdown4 ? <Dropdown4 setdropdown4={setdropdown4} /> : null}
-      {dropdown5 ? <Dropdown5 setdropdown5={setdropdown5} /> : null}
-      {dropdown6 ? <Dropdown6 setdropdown6={setdropdown6} /> : null}
-      {dropdown7 ? <Dropdown7 setdropdown7={setdropdown7} /> : null}
-      {dropdown8 ? <Dropdown8 setdropdown8={setdropdown8} /> : null}
-      {dropdown9 ? <Dropdown9 setdropdown9={setdropdown9} /> : null}
-      {dropdown10 ? <Dropdown10 setdropdown10={setdropdown10} /> : null}
-      {dropdown11 ? <Dropdown11 setdropdown11={setdropdown11} /> : null}
-      {dropdown12 ? <Dropdown12 setdropdown12={setdropdown12} /> : null}
-    </>
-  );
-};
+//           <Text
+//             onMouseEnter={() => setdropdown12(true)}
+//             onMouseLeave={() => setdropdown12(false)}
+//           >
+//             Sale
+//           </Text>
+//         </Flex>
+//       </nav>
+//       {/* {dropdown ? <Dropdown setdropdown={setdropdown} /> : null} */}
+//       {dropdown1 ? <Dropdown1 setdropdown1={setdropdown1} /> : null}
+//       {dropdown2 ? <Dropdown2 setdropdown2={setdropdown2} /> : null}
+//       {dropdown3 ? <Dropdown3 setdropdown3={setdropdown3} /> : null}
+//       {dropdown4 ? <Dropdown4 setdropdown4={setdropdown4} /> : null}
+//       {dropdown5 ? <Dropdown5 setdropdown5={setdropdown5} /> : null}
+//       {dropdown6 ? <Dropdown6 setdropdown6={setdropdown6} /> : null}
+//       {dropdown7 ? <Dropdown7 setdropdown7={setdropdown7} /> : null}
+//       {dropdown8 ? <Dropdown8 setdropdown8={setdropdown8} /> : null}
+//       {dropdown9 ? <Dropdown9 setdropdown9={setdropdown9} /> : null}
+//       {dropdown10 ? <Dropdown10 setdropdown10={setdropdown10} /> : null}
+//       {dropdown11 ? <Dropdown11 setdropdown11={setdropdown11} /> : null}
+//       {dropdown12 ? <Dropdown12 setdropdown12={setdropdown12} /> : null}
+//     </>
+//   );
+// };
 
-export default Navbar;
+// export default Navbar;
